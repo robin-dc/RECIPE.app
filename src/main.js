d07caca76a991e0ee98c372a8b1c168d4ab8ca40
robin-dc <robindelacruz725@gmail.com>
2023-06-15 00:17:18 +0800

    change mobile responsiveness

--- src/main.js
+++ src/main.js
@@ -22,7 +22,7 @@ if(localStorageBrowser !== null){
 
 // localStorage.removeItem("Favorites")
 // =================================================== DATA FETCHERS =================================================
-async function renderCategories(){
+async function getCategoriesData(){
     try{
         const response = await fetch('https://www.themealdb.com/api/json/v1/1/categories.php')
         const data = await response.json()
@@ -32,9 +32,9 @@ async function renderCategories(){
                         <div class="relative bg-wood">
                             <img src="${category.strCategoryThumb}" class="w-full" alt="">
                         </div>
-                        <div class="p-1 flex flex-col justify-between h-full">
-                            <h3 class="text-[1.5rem] md:text-2xl font-bold text-black">${category.strCategory}</h3>
-                            <p class="text-sm md:text-base text-gray-700">${shortenDescription(category.strCategoryDescription)}</p>
+                        <div class="p-[0.8rem] lg:p-1 flex flex-col justify-between h-full">
+                            <h3 class="text-[1.5rem] lg:text-2xl font-bold text-black">${category.strCategory}</h3>
+                            <p class="text-sm lg:text-base text-gray-700">${shortenDescription(category.strCategoryDescription)}</p>
                             <div class="text-center md:text-left"><button class="w-full md:w-fit text-sm md:text-base rounded-lg text-black border-2 border-black font-bold py-[0.5rem] px-1 mt-1 md:mt-2" id="view-category-btn" data-id="${category.strCategory}">View ${category.strCategory} Meals</button></div>
 
                         </div>
@@ -71,11 +71,11 @@ function renderMeals(array, category){
                             <img src="${meal.strMealThumb}" class="w-full h-full object-cover" alt="">
                             <button class="rounded-full bg-white w-2.5 h-2.5 absolute top-1 right-1 flex justify-center items-center shadow-md cursor-pointer" id="heart-btn"><i class="fa-solid fa-heart transition-all ease-in-out duration-500 text-gray-300 text-[1.2rem]" data-id="${meal.idMeal}"></i></button>
                         </div>
-                        <div class="p-1 flex flex-col justify-between h-full">
-                            <h3 class="text-[1rem] md:text-[1.4rem] font-bold text-black">${shortenTitle(meal.strMeal)}</h3>
-                            <div class="flex flex-col md:flex-row justify-center md:justify-between items-center mt-[0.5rem] md:mt-1 gap-[0.3rem] md:gap-0">
+                        <div class="p-[0.8rem] lg:p-1 flex flex-col justify-between h-full">
+                            <h3 class="text-[1rem] md:text-[1.2rem] lg:text-[1.4rem] font-bold text-black">${shortenTitle(meal.strMeal)}</h3>
+                            <div class="flex flex-col lg:flex-row justify-center md:justify-between items-center mt-[0.5rem] md:mt-1 gap-[0.3rem] md:gap-0">
                                 <small class="block text-base text-gray-500"><i class="fa-regular fa-clock mr-[0.4rem]"></i>30 Minutes</small>
-                                <button class="w-full md:w-fit rounded-lg text-gray-600 border-2 border-gray-600 bg-white py-[0.5rem] px-1 font-bold" id="view-recipe-btn" data-id="${meal.strMeal}">View Recipe</button>
+                                <button class="w-full lg:w-fit rounded-lg text-gray-600 border-2 border-gray-600 bg-white py-[0.5rem] px-1 font-bold" id="view-recipe-btn" data-id="${meal.strMeal}">View Recipe</button>
                             </div>
                         </div>
                     </div>`
@@ -85,7 +85,7 @@ function renderMeals(array, category){
         // ==================================== EVENT LISTENERS IN THE MEALS SECTION =================================
         document.getElementById('back-category-btn').addEventListener('click', () => {
             foodContainer.innerHTML = svg
-            renderCategories()
+            getCategoriesData()
         })
         const heartBtn = document.querySelectorAll('#heart-btn');
         heartBtn.forEach(btn => {
@@ -140,31 +140,31 @@ async function getRecipeData(meal) {
         const data = await response.json()
         const object = data.meals[0]
 
-        foodContainer.innerHTML = `<button class="rounded-full bg-white shadow-lg cursor-pointer border border-gray-200 w-3 h-3 absolute top-0 left-1 md:left-0 flex justify-center items-center" id="back-meals-btn"><i class="fa-solid fa-arrow-left text-black"></i></button>
-        <div class="col-span-4 flex flex-col gap-1 md:gap-3">
-            <div class="rounded-2xl shadow-xl pt-2 p-1 md:p-3 leading-[3rem] border border-gray-200" >
-                <h1 class="text-3xl md:text-4xl font-bold text-lightBrown">${object.strMeal}</h1>
-                <p class="text-[1.4rem] md:text-2xl leading-[2rem] md:leading-[3rem] tracking-wide text-navyBlue">
+        foodContainer.innerHTML = `<button class="rounded-full bg-white shadow-lg cursor-pointer border border-gray-200 w-3 h-3 absolute top-0 left-1 lg:left-0 flex justify-center items-center" id="back-meals-btn"><i class="fa-solid fa-arrow-left text-black"></i></button>
+        <div class="col-span-4 flex flex-col gap-1 lg:gap-3">
+            <div class="rounded-2xl shadow-xl pt-2 p-1 lg:p-3 leading-[3rem] border border-gray-200" >
+                <h1 class="text-3xl lg:text-4xl font-bold text-lightBrown">${object.strMeal}</h1>
+                <p class="text-[1.3rem] lg:text-2xl leading-[2rem] lg:leading-[3rem] tracking-wide text-navyBlue">
                     Meal originated from ${object.strArea} folks
                 </p>
-                <div class="w-full h-[25rem] mt-2 mb-2 md:mb-4 bg-black rounded-tr-[5rem] rounded-bl-[5rem] rounded-3xl overflow-hidden">
+                <div class="w-full h-[18rem] lg:h-[25rem] mt-2 mb-2 lg:mb-4 bg-black rounded-tr-[5rem] rounded-bl-[5rem] rounded-3xl overflow-hidden">
                     <img src="${object.strMealThumb}" class="w-full h-full object-cover opacity-80" alt="">
                 </div>
                 <hr>
-                <div class="my-2 md:my-4">
-                    <h2 class="text-center text-3xl font-bold">Ingredients</h2>
-                    <ul class="w-3/5 mx-auto mt-3 flex flex-col md:flex-row gap-[0.5rem] md:gap-2 md:flex-wrap list-disc text-xl">
+                <div class="my-2 lg:my-4">
+                    <h2 class="text-center text-2xl lg:text-3xl font-bold">Ingredients</h2>
+                    <ul class="w-3/5 mx-auto mt-2 lg:mt-3 flex flex-col md:flex-row gap-[0.5rem] md:gap-2 md:flex-wrap list-disc text-xl">
                         ${renderIngredients(object)}
                     </ul>
                 </div>
             </div>
-            <div class="grid grid-cols-1 md:grid-cols-4 gap-[0.5rem] md:gap-2 mt-3">
-                <h2 class="text-center text-3xl font-bold col-span-1 md:col-span-4 mb-1">Instructions</h2>
+            <div class="grid grid-cols-1 lg:grid-cols-4 gap-[0.5rem] lg:gap-2 mt-3">
+                <h2 class="text-center text-2xl lg:text-3xl font-bold col-span-1 lg:col-span-4 mb-1">Instructions</h2>
                     ${renderInstructions(object.strInstructions)}
             </div>
             <div class="mt-3 p-0 md:p-2">
-                <h2 class="text-center text-3xl font-bold">Video Tutorial</h2>
-                <iframe class="w-full h-[23rem] md:h-[35rem] my-2 md:my-3 rounded-2xl" src="https://www.youtube.com/embed/${getEmbeddedCode(object.strYoutube)}" frameborder="0" allowfullscreen></iframe>
+                <h2 class="text-center text-2xl lg:text-3xl font-bold">Video Tutorial</h2>
+                <iframe class="w-full h-[23rem] lg:h-[35rem] my-2 md:my-3 rounded-2xl" src="https://www.youtube.com/embed/${getEmbeddedCode(object.strYoutube)}" frameborder="0" allowfullscreen></iframe>
             </div>
         </div>`
         document.getElementById('back-meals-btn').addEventListener('click', () => {
@@ -195,11 +195,11 @@ searchBtn.addEventListener('click', (e) => {
 })
 categoriesBtn.addEventListener('click', () => {
     foodContainer.innerHTML = svg
-    renderCategories()
+    getCategoriesData()
 })
 categoriesHeroBtn.addEventListener('click', () => {
     foodContainer.innerHTML = svg
-    renderCategories()
+    getCategoriesData()
 })
 cheeseBurger.addEventListener('click', () => {
     foodContainer.innerHTML = svg
@@ -248,11 +248,11 @@ function renderFavoriteMeal(meal,length){
                         <img src="${meal.strMealThumb}" class="w-full h-full object-cover" alt="">
                         <button class="rounded-full bg-white w-2.5 h-2.5 absolute top-1 right-1 flex justify-center items-center shadow-md cursor-pointer" id="heart-btn"><i class="fa-solid fa-heart transition-all ease-in-out duration-500 text-red-500 text-[1.2rem]" data-id="${meal.idMeal}"></i></button>
                     </div>
-                    <div class="p-1 flex flex-col justify-between h-full">
-                        <h3 class="text-[1rem] md:text-[1.4rem] font-bold text-black">${shortenTitle(meal.strMeal)}</h3>
-                        <div class="flex flex-col md:flex-row justify-center md:justify-between items-center mt-[0.5rem] md:mt-1 gap-[0.3rem] md:gap-0">
+                    <div class="p-[0.8rem] lg:p-1 flex flex-col justify-between h-full">
+                        <h3 class="text-[1rem] md:text-[1.2rem] lg:text-[1.4rem] font-bold text-black">${shortenTitle(meal.strMeal)}</h3>
+                        <div class="flex flex-col lg:flex-row justify-center md:justify-between items-center mt-[0.5rem] md:mt-1 gap-[0.3rem] md:gap-0">
                             <small class="block text-base text-gray-500"><i class="fa-regular fa-clock mr-[0.4rem]"></i>30 Minutes</small>
-                            <button class="w-full md:w-fit rounded-lg text-gray-600 border-2 border-gray-600 bg-white py-[0.5rem] px-1 font-bold" id="view-recipe-btn" data-id="${meal.strMeal}">View Recipe</button>
+                            <button class="w-full lg:w-fit rounded-lg text-gray-600 border-2 border-gray-600 bg-white py-[0.5rem] px-1 font-bold" id="view-recipe-btn" data-id="${meal.strMeal}">View Recipe</button>
                         </div>
                     </div>
                 </div>`
@@ -322,7 +322,7 @@ function renderIngredients(data){
 
     for(item in data){
         if(item.startsWith("strIngredient") && data[item]){
-            ingredients.push(`<li><strong class="text-red-700">${data['strMeasure'+ count]}</strong> ${data[item]}</li>`)
+            ingredients.push(`<li class="text-[0.9rem] lg:text-base"><strong class="text-red-700">${data['strMeasure'+ count]}</strong> ${data[item]}</li>`)
             count++;
         }
     }
@@ -341,8 +341,8 @@ function renderInstructions(data){
                 i += 2
             }
             else{
-                steps.push(`<div class="rounded-2xl shadow-xl p-2 bg-wood text-white h-fit md:h-full w-full">
-                            <h4 class="text-[1.4rem] md:text-2xl font-bold mb-1">Step ${steps.length+1}</h4>
+                steps.push(`<div class="rounded-2xl shadow-xl p-1 lg:p-2 bg-wood text-white h-fit lg:h-full w-full">
+                            <h4 class="text-[1.3rem] md:text-[1.5rem] lg:text-2xl font-bold mb-[0.7rem] lg:mb-1">Step ${steps.length+1}</h4>
                             <p>${str}</p>
                         </div>`)
                 str = ""
@@ -351,8 +351,8 @@ function renderInstructions(data){
 
         }
         if(period == 2 && str.length >= 100 && isNaN(Number(data[i])) && data[i+1] !== "."){
-            steps.push(`<div class="rounded-2xl shadow-xl p-2 bg-wood text-white h-fit md:h-full w-full">
-                        <h4 class="text-[1.4rem] md:text-2xl font-bold mb-1">Step ${steps.length+1}</h4>
+            steps.push(`<div class="rounded-2xl shadow-xl p-1 lg:p-2 bg-wood text-white h-fit lg:h-full w-full">
+                        <h4 class="text-[1.3rem] md:text-[1.5rem] lg:text-2xl font-bold mb-[0.7rem] lg:mb-1">Step ${steps.length+1}</h4>
                         <p>${str}</p>
                     </div>`)
             period = 0;
@@ -380,16 +380,16 @@ function getEmbeddedCode(link){
     }
 }
 function foodError(){
-    foodContainer.innerHTML = `<p class="fixed top-[40%] left-[50%] right-[50%] translate-x-[-50%] text-gray-400 text-[1.6rem] md:text-2xl font-semibold block w-full text-center">The food you are craving cannot be found.</p>`
+    foodContainer.innerHTML = `<p class="absolute top-[30%] left-[50%] right-[50%] translate-x-[-50%] text-gray-400 text-[1.6rem] md:text-2xl font-semibold block w-full text-center">The food you are craving cannot be found.</p>`
 }
 function foodMissing(){
-    foodContainer.innerHTML = `<p class="fixed top-[40%] left-[50%] right-[50%] translate-x-[-50%] text-gray-400 text-[1.6rem] md:text-2xl font-semibold block w-full text-center">The search bar is empty.</p>`
+    foodContainer.innerHTML = `<p class="absolute top-[30%] left-[50%] right-[50%] translate-x-[-50%] text-gray-400 text-[1.6rem] md:text-2xl font-semibold block w-full text-center">The search bar is empty.</p>`
 }
 function favoriteError(){
-    foodContainer.innerHTML = `<p class="fixed top-[40%] left-[50%] right-[50%] translate-x-[-50%] text-gray-400 text-[1.6rem] md:text-2xl font-semibold block w-full text-center">Your favorite foods is empty.</p>`
+    foodContainer.innerHTML = `<p class="absolute top-[30%] left-[50%] right-[50%] translate-x-[-50%] text-gray-400 text-[1.6rem] md:text-2xl font-semibold block w-full text-center">Your favorite foods is empty.</p>`
 }
 function backBtn(category){
-    return `<button class="rounded-full bg-white shadow-lg cursor-pointer border border-gray-200 px-1 h-3 absolute top-0 md:left-0 left-1 flex justify-center items-center gap-[0.5rem]" id="back-category-btn"><i class="fa-solid fa-arrow-left text-black"></i><span class="font-semibold">${category} Category</span></button>`
+    return `<button class="rounded-full bg-white shadow-lg cursor-pointer border border-gray-200 px-1 h-3 absolute top-0 lg:left-0 left-1 flex justify-center items-center gap-[0.5rem]" id="back-category-btn"><i class="fa-solid fa-arrow-left text-black"></i><span class="font-semibold">${category} Category</span></button>`
 }
 
 function scrollToTop(){
